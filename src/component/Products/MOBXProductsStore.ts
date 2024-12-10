b5f3eb740d291bf16585fc3f0600e4a9a5c9b314
AdaDev11 <azadasltnv@gmail.com>
2024-12-10 12:32:39 +0500

    Update MOBXProductsStore.ts

--- src/component/Products/MOBXProductsStore.ts
+++ src/component/Products/MOBXProductsStore.ts
@@ -1,75 +1,12 @@
-import { makeAutoObservable } from "mobx";
-import axios from "axios";
 
-export interface Product {
-  id: number;
-  title: string;
-  category: string;
-  price: number;
-  images: string[];
-  description: string;
-}
 
-class ProductStore {
-  products: Product[] = [];
-  isLoading: boolean = false;
-  totalProducts: number = 0;
-  limit: number = 4;
-  skip: number = 0;
-  searchQuery: string = "";
-  selectedCategory: string | null = "";
-  cart: {product: Product, quantity: number}[] = [];
-
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  async fetchProducts() {
+  async filteredPrice (minPrice, maxPrice) {
     this.isLoading = true;
     try {
-      const response = await axios.get(
-        `https://dummyjson.com/products?limit=${this.limit}&skip=${this.skip}&select=title,price,description,category,images`
-      );
-      this.products = response.data.products;
-      this.totalProducts = response.data.total;
-    } catch (error) {
-      console.error("Error fetch:", error);
-    } finally {
-      this.isLoading = false;
+      const res = await axios.get(`https://dummyjson.com/products?minPrice=${minPrice}&maxPrice=${maxPrice}`)
+      this.products = res.data.products;
+      console.log(res + "jhknm " + this.products);
+    } catch {
+      console.error("Price filter error: " + error);
     }
   }
-
-  setPage(page: number) {
-    this.skip = (page - 1) * this.limit;
-    this.fetchProducts();
-  }
-
-  addToCart = (product: Product) => {
-    const item = this.cart.find((cartItem) => cartItem.product.id === product.id);
-    console.log(this.cart);
-    if(item) {
-      item.quantity += 1;
-    }else {
-      this.cart.push({product, quantity: 1});
-    }
-  };
-
-  get totalPrice () {
-    return this.cart.reduce((sum, item) => sum + item.product.price * item.quantity,0);
-  };
-
-  changeQuantity = (id: number, newQuantity: number) => {
-    const item = this.cart.find((cartItem) => cartItem.product.id === id);
-    if(item) {
-      item.quantity = newQuantity;
-    }
-  };
-
-  removeFromCart = (id: number) => {
-    this.cart = this.cart.filter((cartItem) => cartItem.product.id !== id)
-  };
-
-}
-
-const productStore = new ProductStore();
-export default productStore;
